bbf23afd953aec7e9cb83bddee3abf35fdb569ee
ianchan3 <ianchan3138@gmail.com>
2023-03-28 14:36:35 -0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './pages';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
-    {/* <Header />
+    <Header />
     <AboutUs />
     <SpecialMenu />
     <Chef />
@@ -15,7 +15,7 @@ const App = () => (
     <Laurels />
     <Gallery />
     <FindUs />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
