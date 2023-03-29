1eb0630376beaed3597cc517118a09a29181c1b2
ianchan3 <ianchan3138@gmail.com>
2023-03-29 14:04:21 -0700

    Chef Section Completed

--- src/pages/Chef/Chef.jsx
+++ src/pages/Chef/Chef.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import {images} from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className='app__wrapper_img app__wrapper_img reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word"/>
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolore commodi possimus sint? Doloribus autem odio illo quis? Recusandae ipsam eaque hic reiciendis nisi cumque numquam delectus odit ipsum dicta.</p>
+        </div>
+        <p className='p__opensans'></p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Gordon Ramsey</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
