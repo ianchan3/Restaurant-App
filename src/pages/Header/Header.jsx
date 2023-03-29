a08967df6059bd76110c13ee8409b45b75583e80
ianchan3 <ianchan3138@gmail.com>
2023-03-29 12:58:13 -0700

    About Us Page Created

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -6,10 +6,13 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavor"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}></p>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
-      
+      <img src={images.welcome} alt="header-img" />
     </div>
   </div>
 );
